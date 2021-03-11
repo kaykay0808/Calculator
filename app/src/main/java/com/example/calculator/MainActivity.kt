5bfc59e9286f8a69c5f6315961644949c681a77d
kay <kayshiro@live.no>
2021-03-11 21:13:22 +0100

    decrement and increment

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculator.databinding.ActivityMainBinding
 
+//this is the class Jason is talking about
 class MainActivity : AppCompatActivity() {
+
+    //når appen starter får verdien en deafult nummer som kan forandres
+    private var numberOfClients: Int = 0; //variable name
+    private var numberOfWorkDays: Int = 20; //variable name
+    //buttons
+
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // set binding.(and the ID here?)
+
+
+        initializeUi() // A part of function 2, we need to call the function from outside so it always start
+
     }
+    //function 2
+    //This function calls the initializeUi() inside oncreate
+    private fun initializeUi() {
+        // converts the views to string
+        binding.clientcount.text = numberOfClients.toString() // ID.text = variable name > converts to string
+        binding.daysleftcount.text = numberOfWorkDays.toString() // ID.text = variable name > converts to string
+
+        // - and + symbols
+        binding.substractClientImage.setOnClickListener {
+            //find the function for decrement
+            numberOfClients--;
+            // Display the new value in the text view.
+            binding.clientcount.text = numberOfClients.toString()
+        }
+        binding.addClientImage.setOnClickListener {
+            //find the function for increment
+            numberOfClients++;
+            // Display the new value in the text view.
+            binding.clientcount.text = numberOfClients.toString()
+        }
+    }
+
 }
